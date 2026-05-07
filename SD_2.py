9aa65e66faa0a301d7e438b37dc45bc206cb823d
JaneLeeAug <132145188+JaneLeeAug@users.noreply.github.com>
2026-05-07 16:03:07 +0800

    Create SD_2.py

---
+++ SD_2.py
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+TMP_INIT=$(mktemp /tmp/init1.XXXXXXXXXXXXXXXXXXXXXXXXXXX)
+
+if [ -f ~/.bashrc ]; then
+    echo "source ~/.bashrc" > "$TMP_INIT"
+fi
+
+echo "set -m" >> "$TMP_INIT"
+
+echo '
+cd /proj/gpu_d_09023/mtk53732/BreezyVoice
+
+start_part=48
+
+for gpu in {0..7}
+do
+    for local_idx in {0..5}
+    do
+        part=$((start_part + gpu * 6 + local_idx))
+        part_str=$(printf "%03d" "$part")
+
+        CUDA_VISIBLE_DEVICES=$gpu python batch_inference.py \
+            --model_path /proj/gpu_d_09023_MR_dataset_ARCHIVE/mtk53732/pretrained_models/BreezyVoice-300M/ \
+            --csv_file /proj/gpu_d_09023_MR_dataset_ARCHIVE/mtk53732/pairingTable/pairing_part_${part_str}.csv \
+            --speaker_prompt_audio_folder /proj/gpu_d_09023_MR_dataset_ARCHIVE/mtk53732/speakerWav/ \
+            --output_audio_folder /proj/gpu_d_09023/mtk53732/SD \
+            > SD_${part_str}.txt 2>&1 &
+
+    done
+done
+' >> "$TMP_INIT"
+
+exec bash --rcfile "$TMP_INIT" -i
