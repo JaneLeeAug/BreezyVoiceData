583e6c6259ce7f93a6495cda0587e4f91b7c4733
JaneLeeAug <132145188+JaneLeeAug@users.noreply.github.com>
2026-05-11 15:25:19 +0800

    Update 3dspeaker_terminal.sh

--- 3dspeaker_terminal.sh
+++ 3dspeaker_terminal.sh
@@ -1,3 +1,10 @@
 python speakerlab/bin/infer_sv.py \
   --model_id iic/speech_campplus_sv_zh-cn_16k-common \
   --wavs prompt_16k.wav generated_16k.wav
+
+
+python speakerlab/bin/infer_sv.py \
+  --model_id iic/speech_campplus_sv_zh-cn_16k-common \
+  --wavs /proj/MR_dataset/mtk53732/SDSD0003640036.wav /proj/MR_dataset/mtk53732/SD/SD0002567077.wav
+
+  
